8181d2a8c1176915b1f7773c0b1df05fed1fd24f
Developer-EJ <155644012+Developer-EJ@users.noreply.github.com>
2026-05-05 16:28:30 +0900

    argument passing 통과

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -8,8 +8,8 @@
 #include "threads/flags.h"
 #include "intrinsic.h"
 
-void syscall_entry (void);
-void syscall_handler (struct intr_frame *);
+void syscall_entry(void);
+void syscall_handler(struct intr_frame *);
 
 /* System call.
  *
@@ -20,27 +20,69 @@ void syscall_handler (struct intr_frame *);
  * The syscall instruction works by reading the values from the the Model
  * Specific Register (MSR). For the details, see the manual. */
 
-#define MSR_STAR 0xc0000081         /* Segment selector msr */
-#define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
+#define MSR_STAR 0xc0000081			/* Segment selector msr */
+#define MSR_LSTAR 0xc0000082		/* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
-void
-syscall_init (void) {
-	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
-			((uint64_t)SEL_KCSEG) << 32);
-	write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
+void syscall_init(void)
+{
+	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48 |
+							((uint64_t)SEL_KCSEG) << 32);
+	write_msr(MSR_LSTAR, (uint64_t)syscall_entry);
 
 	/* The interrupt service rountine should not serve any interrupts
 	 * until the syscall_entry swaps the userland stack to the kernel
 	 * mode stack. Therefore, we masked the FLAG_FL. */
 	write_msr(MSR_SYSCALL_MASK,
-			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+			  FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
 
 /* The main system call interface */
-void
-syscall_handler (struct intr_frame *f UNUSED) {
+void syscall_handler(struct intr_frame *f UNUSED)
+{
 	// TODO: Your implementation goes here.
-	printf ("system call!\n");
-	thread_exit ();
+	switch (f->R.rax)
+	{
+	case SYS_WRITE:
+		// 반환값이 있는 시스템콜은 다시 rax에 저장
+		// 일단 write가 구현중이므로 터미널 출력으로 고정
+		f->R.rax = write(1, (void *)f->R.rsi, f->R.rdx);
+		break;
+	case SYS_EXIT:
+		exit(f->R.rdi);
+		break;
+	case SYS_HALT:
+		halt();
+		break;
+	default:
+		thread_exit();
+	}
+}
+
+int write(int fd, const void *buffer, unsigned size)
+{
+	if (fd == 1)
+	{
+		putbuf(buffer, size);
+		return size;
+	}
+
+	// 일단 argu pass 구현하고 다음에..
+	// // fd를 통해 파일 객체 가져오기
+	// struct file *file =
+	// 	if (file == NULL) return -1;
+
+	// return file_write(file, buffer, size);
+}
+
+void exit(int status)
+{
+	// 종료 코드 저장
+	thread_current()->exit_code = status;
+	thread_exit();
+}
+
+void halt(void)
+{
+	power_off();
 }
